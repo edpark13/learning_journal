a48a5efc045d27c63b1a9ecee5550292d9e533c2
Edward <edpark13@gmail.com>
2015-02-18 13:34:01 -0800

    add json renderer and got form.js to work

--- static/form.js
+++ static/form.js
@@ -1,36 +1,17 @@
 $(document).ready( function() {
-    $('#submit').click( function() {
+    $('#add_entry').submit( function(event) {
         event.preventDefault();
         var title = $('input[name=title]').val();
         var text = $('textarea#text').val();
-        // alert(title + " and " + text);
-        // var request = $.post( "add", function() {
-        //     alert( "success" );
-        // })
         $.ajax({
             url : "add",
             type : "POST",
-            dataType : "json"
+            dataType : "json",
             data : { 
             'title' : title, 
             'text' : text },
-        }).done(function( data ) {
-        alert(data)
+        }).done(function(data) {
+            alert("title = " + data.title);
+        })
     });
-  });
-    //     }).done(get_entries(text))
-    //     // .done(function( msg ) {
-    //     //     alert( "Data Saved: " + msg );
-    //     // });
-    // });
-    // function get_entries(text) {
-    //     // alert("works!")
-    //     var entries = $.ajax({
-    //         url : "home",
-    //         type : "GET",
-    //     }).done(function(msg) {
-    //         alert(msg);
-    //     });
-    //     // get_entry(entries, text)
-    // }
 });
