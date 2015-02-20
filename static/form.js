96dc26b607a53df34a263f638748ffd2274220ab
Edward <edpark13@gmail.com>
2015-02-19 17:52:34 -0800

    add twitter feature

--- static/form.js
+++ static/form.js
@@ -16,6 +16,7 @@ $(document).ready( function() {
                 "class" : "entry",
                 "id" : data.id
             });
+            var twitter = '<a href="https://twitter.com/share" class="twitter-share-button" data-text='+ data.title +' data-via="edpark13" data-size="large" data-count="none">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\'://platform.twitter.com/widgets.js\';fjs.parentNode.insertBefore(js,fjs);}}(document, \'script\', \'twitter-wjs\');</script>'
             var title =  $('<h3></h3>').text(data.title);
             var date = $('<p>').addClass("dateline").text(data.created);
             var text = $('<div></div>').addClass("entry_body").html(data.text);
@@ -23,8 +24,13 @@ $(document).ready( function() {
             link_button.append($('<button></button>').attr("type", "button").text("permalink"));
             var edit_button = $('<a></a>').attr('href', 'edit/' + data.id);
             edit_button.append($('<button></button>').attr("type", "button").text("edit"));
-            id.append(title, date, text, link_button, edit_button);
+            id.append(title, date, text, link_button, edit_button, twitter);
             $('#entries').prepend(id);
+            twttr.widgets.load()
         })
-    });    
+    });
+
+    function tweet(data) {
+        $('#form').hide();
+    }  
 });
